f7d7ff59831f812fdbccec4db7650e7e5a091a92
Bryan Villanueva <Alexander06059766@outlook.com>
2019-09-22 14:39:51 -0600

    Agregar Elementos

---
+++ Notas_Fire/Notas_Fire/Vista/Lista_Contenido.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notas_Fire.Vista
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Lista_Contenido : ContentPage
+    {
+        public Lista_Contenido()
+        {
+            InitializeComponent();
+        }
+    }
+}
